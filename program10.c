d3f9e5975494e635ce29a76939e1204855f2f914
Varshakatakamsetty <165256885+Varshakatakamsetty@users.noreply.github.com>
2025-02-15 14:59:32 +0530

    Update program10.c
    emo chesam anna maku telvadu

--- program10.c
+++ program10.c
@@ -10,7 +10,7 @@ int main() {
     int arr[n];
     printf("Enter the elements: ");
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", arr[i]);
         sum += arr[i];
     }
     printf("Sum of the array elements is %d\n", sum);
